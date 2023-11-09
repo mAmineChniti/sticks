0faef4494a308aa4a9237a534c44d4bf7933868e
mAmineChniti <emin.chniti@esprit.tn>
2023-11-10 00:17:21 +0100

    added update subcommand

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Function to display a progress bar
 function progress_bar {
     local duration=$1
@@ -18,6 +20,40 @@ function progress_bar {
     done
     echo -ne "\n"
 }
+
+# Function to compare version numbers
+function version_gt {
+    local ver1=$1
+    local ver2=$2
+
+    IFS=. read -ra ver1 <<< "$ver1"
+    IFS=. read -ra ver2 <<< "$ver2"
+
+    for ((i = 0; i < ${#ver1[@]}; i++)); do
+        if [ -z "${ver2[i]}" ]; then
+            # If ver2 has fewer components, consider it smaller
+            return 0
+        elif [ "${ver1[i]}" -gt "${ver2[i]}" ]; then
+            return 1
+        elif [ "${ver1[i]}" -lt "${ver2[i]}" ]; then
+            return 0
+        fi
+    done
+
+    return 0
+}
+
+# Get the version from the output of sticks -v
+local_version=$(sticks -v | cut -d ' ' -f 2)
+
+# Fetch the version from Cargo.toml in the repository
+cargo_toml_version=$(curl -s https://raw.githubusercontent.com/mAmineChniti/sticks/master/Cargo.toml | grep "version" | cut -d '"' -f 2)
+
+# Check if the local version is the latest
+if version_gt "$local_version" "$cargo_toml_version"; then
+    echo "Latest version of sticks is already installed."
+    exit 0
+fi
 
 # Check if Rust is installed
 if ! command -v rustc &>/dev/null; then
@@ -42,7 +78,7 @@ echo "Creating a temporary directory..."
 progress_bar 3
 
 # Clone the Git repository
-git clone https://github.com/mAmineChniti/sticks > /dev/null 2>&1
+git clone https://github.com/mAmineChniti/sticks > /dev/null 2>&1 || { echo "Error: Unable to clone the Git repository."; exit 1; }
 
 # Display progress bar for repository cloning
 echo "Cloning the Git repository..."
@@ -55,69 +91,47 @@ cd sticks
 if ! dpkg -l | grep -q "build-essential"; then
     # Install build-essential if not already installed (for Debian/Ubuntu)
     echo "Installing build-essential..."
-    sudo apt update > /dev/null 2>&1
-    sudo apt install build-essential -y > /dev/null 2>&1
+    sudo apt install build-essential -y || { echo "Error: Unable to install build-essential."; exit 1; }
     progress_bar 10
 fi
 
 # Build the project with Cargo in release mode
 echo "Building the project with Cargo..."
-cargo build --release > /dev/null 2>&1
+cargo build --release || { echo "Error: Cargo build failed."; exit 1; }
 progress_bar 10
 
 # Install cargo-deb if not already installed
 if ! command -v cargo-deb &>/dev/null; then
     echo "Installing cargo-deb..."
-    cargo install cargo-deb > /dev/null 2>&1
+    cargo install cargo-deb || { echo "Error: Unable to install cargo-deb."; exit 1; }
     progress_bar 10
 fi
 
-# Detect the OS name
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    os_name=$ID
-else
-    os_name=$(uname -s | tr '[:upper:]' '[:lower:]')
-fi
-
 # Build a package using cargo-deb for the detected OS
-echo "Building a package for $os_name using cargo-deb..."
-cargo deb --target "$host" > /dev/null 2>&1
+echo "Building a package for $host using cargo-deb..."
+cargo deb --target "$host" || { echo "Error: Cargo deb failed."; exit 1; }
 progress_bar 10
 
 # Check if the package is actually generated
-if [ -f "./target/${host}/${os_name}"/*.deb ] || [ -f "./target/${host}"/debian/*.deb ]; then
+if [ -f "./target/${host}/${host}"/*.deb ] || [ -f "./target/${host}"/debian/*.deb ]; then
     # Install the generated package using the appropriate package manager
     echo "Installing the generated package..."
-    case $os_name in
-        debian | raspbian)
-            if [ -f "./target/${host}/${os_name}"/*.deb ]; then
-                sudo apt install "./target/${host}/${os_name}"/*.deb -y > /dev/null 2>&1
-            elif [ -f "./target/${host}"/debian/*.deb ]; then
-                sudo apt install "./target/${host}"/debian/*.deb -y > /dev/null 2>&1
-            else
-                echo "No .deb package found for $os_name."
-            fi
-            ;;
-        ubuntu)
-            if [ -f "./target/${host}/${os_name}"/*.deb ]; then
-                sudo apt install "./target/${host}/${os_name}"/*.deb -y > /dev/null 2>&1
-            elif [ -f "./target/${host}"/debian/*.deb ]; then
-                sudo apt install "./target/${host}"/debian/*.deb -y > /dev/null 2>&1
-            else
-                echo "No .deb package found for $os_name."
-            fi
-            ;;
-        fedora)
-            sudo dnf install "./target/${host}/${os_name}"/*.rpm -y > /dev/null 2>&1
-            ;;
-        centos | rhel)
-            sudo yum install "./target/${host}/${os_name}"/*.rpm -y > /dev/null 2>&1
+    case $host in
+        x86_64-unknown-linux-gnu)
+            os_name="debian"
             ;;
         *)
-            echo "Unsupported OS: $os_name. Please install the package manually."
+            echo "Unsupported OS: $host. Please install the package manually."
             ;;
     esac
+
+    if [ -f "./target/${host}/${host}"/*.deb ]; then
+        sudo apt install "./target/${host}/${host}"/*.deb -y || { echo "Error: Unable to install the generated package."; exit 1; }
+    elif [ -f "./target/${host}"/debian/*.deb ]; then
+        sudo apt install "./target/${host}"/debian/*.deb -y || { echo "Error: Unable to install the generated package."; exit 1; }
+    else
+        echo "No .deb package found for $host."
+    fi
 else
     echo "Package not generated. Please check the build process."
 fi
