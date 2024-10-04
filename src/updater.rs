3e14149ed4e3e886478d308e31a62cc5f6d7d99b
mAmineChniti <emin.chniti@esprit.tn>
2024-10-04 14:06:31 +0100

    added update for windows

--- src/updater.rs
+++ src/updater.rs
@@ -1,15 +1,34 @@
 // updater.rs
 use std::io::Result;
 
-const UPDATE_SCRIPT_URL: &str = "https://rb.gy/ltig1b";
+const UPDATE_SCRIPT_URL_LINUX: &str = "https://rebrand.ly/tyzot1g";
+const UPDATE_SCRIPT_URL_WINDOWS: &str = "https://rebrand.ly/rcq5ijm";
 
-/// Updates the project by executing an update script.
 pub fn update_project() -> Result<()> {
-	let update_command = format!("curl -fsSL {} | bash", UPDATE_SCRIPT_URL);
-	let status = std::process::Command::new("sh")
-		.arg("-c")
-		.arg(&update_command)
-		.status()?;
+	let os = std::env::consts::OS;
+
+	let update_command = match os {
+		"linux" | "macos" => format!("curl -fsSL {} | bash", UPDATE_SCRIPT_URL_LINUX),
+		"windows" => format!(
+			"powershell -Command \"& {{ iwr -useb {} | iex }}\"",
+			UPDATE_SCRIPT_URL_WINDOWS
+		),
+		_ => {
+			eprintln!("Unsupported operating system: {}", os);
+			return Ok(());
+		}
+	};
+
+	let status = if os == "windows" {
+		std::process::Command::new("cmd")
+			.args(["/C", &update_command])
+			.status()?
+	} else {
+		std::process::Command::new("sh")
+			.arg("-c")
+			.arg(&update_command)
+			.status()?
+	};
 
 	if status.success() {
 		println!("Update successful!");
