f20f66c537a4f188b4299709134b2b8ff4a7de54
mAmineChniti <emin.chniti@esprit.tn>
2024-11-15 11:34:05 +0100

    bash instead of sh in update

--- src/updater.rs
+++ src/updater.rs
@@ -1,12 +1,11 @@
 // updater.rs
 use std::io::Result;
 
-//const UPDATE_SCRIPT_URL_LINUX: &str = "https://rebrand.ly/tyzot1g";
+const UPDATE_SCRIPT_URL_LINUX: &str = "https://rebrand.ly/tyzot1g";
 // const UPDATE_SCRIPT_URL_WINDOWS: &str = "https://rebrand.ly/36j6rhv";
-const UPDATE_SCRIPT_URL_LINUX: &str = "https://raw.githubusercontent.com/mAmineChniti/sticks/master/install.sh";
 
 pub fn update_project() -> Result<()> {
-	let update_command = format!("curl -fsSL {} | sh", UPDATE_SCRIPT_URL_LINUX);
+	let update_command = format!("curl -fsSL {} | bash", UPDATE_SCRIPT_URL_LINUX);
 	let status = std::process::Command::new("sh")
 		.arg("-c")
 		.arg(update_command)
